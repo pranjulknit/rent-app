8702be67104eb03d967149cef6d84b918449c824
Pranjul Chaurasiya <chaurasiyapranjul@gmail.com>
2025-02-03 17:48:45 +0530

    models created

---
+++ server/models/place.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const placeSchema = new mongoose.Schema({
+    owner:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    title: String,
+    address:String,
+    photos: [String],
+    description : String,
+    perks: [String],
+    extraInfo: String,
+    checkIn:Number,
+    checkOut:Number,
+    maxGuests:Number,
+    price:Number,
+})
+
+export const PlaceModel = mongoose.model('Place',placeSchema)
